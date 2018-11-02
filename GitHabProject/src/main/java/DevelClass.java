65045bf7e5875538045be6b9c454359827412d8e
ake <anatoly.ekert@iso-gruppe.com>
2018-11-02 13:03:36 +0100

    erste Class in Devel

---
+++ GitHabProject/src/main/java/DevelClass.java
@@ -0,0 +1,11 @@
+/**
+ * @author ${User} on ${Date}
+ * @project GitHabProject
+ * @CopyRights ISO-GRUPPE GmbH
+ **/
+public class DevelClass {
+
+    public static void main(String[] args) {
+        String str = "Devel";
+    }
+}
